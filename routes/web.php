0ac0b92307077309c7121a384f608c8d454dd64c
Maulida Aprila <maulidaaprila12@gmail.com>
2026-04-26 18:19:44 +0700

    test:test protected branch

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+});
